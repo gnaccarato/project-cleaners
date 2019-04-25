99b6ea5118e5ff91ee5c4b54d6c4db03b8175fa5
gnaccarato <naccarato2005@gmail.com>
2019-04-25 13:20:49 -0400

    responsive design

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const navbar = document.getElementById('navbar');
-const navDrop = document.getElementById('navbar_drop');
+if (window.matchMedia('(max-width: 640px)').matches) { 
+    const navbar = document.getElementById('navbar');
+    const navDrop = document.getElementById('navbar_drop');
 
-navDrop.addEventListener('click', () => {
-    if(navbar.style.display != 'block') {
-        navbar.style.display = 'block';
-        this.innerHTML = 'x';
-    } else {
-        navbar.style.display = 'none';
-        this.innerHTML = '=';
-    }
-})
+    navDrop.addEventListener('click', () => {
+        if(navbar.style.display != 'block') {
+            navbar.style.display = 'block';
+            navDrop.innerHTML = 'x';
+        } else {
+            navbar.style.display = 'none';
+            navDrop.innerHTML = '&#9776';
+        }
+    })
+} 
